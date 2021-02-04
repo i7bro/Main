f7d5de370537692f602110ce5b43ef7e6dd820d7
Ilya Semibratov <50609560+i7bro@users.noreply.github.com>
2021-02-04 18:24:42 +0400

    Update Temp.java

--- src/Temp.java
+++ src/Temp.java
@@ -61,7 +61,6 @@ class Temp {
         FileResource fr = new FileResource("exportdata.csv");
         CSVParser parser = fr.getCSVParser();
         Pars.bigExporters(parser, "$999,999,999,999");
-
     }
 }
 
